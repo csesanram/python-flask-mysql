5c6049ea0c5665899faf1cfd832f44a231e5dc98
Sankaranarayanan Ramasamy <sankaranarayanan.ramasamy3@t-mobile.com>
2019-11-14 17:50:10 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,18 @@
+def Issue():
+    issue = [
+        {
+            'id': 1,
+            'issue': 'java memory issue',
+            'resolution': 'restart the server',
+            'fixedby':'team',
+            'create_date': '11-08-2019'
+        },
+        {
+            'id': 2,
+            'issue': 'url is incorrect',
+            'resolution': 'assign the proper url',
+            'fixedby':' team',
+            'create_date': '11-08-2019'
+        }
+    ]
+    return issue
